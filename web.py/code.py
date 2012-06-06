68f0bc214a5dd057d487ed37a9dd9e4643608153
Jianhong Liu <liujianhong1986@gmail.com>
2012-06-06 23:09:58 +0800

    simple todo

--- web.py/code.py
+++ web.py/code.py
@@ -3,14 +3,23 @@ import web
 render = web.template.render('templates/')
 
 urls = (
-    '/', 'index'
+    '/', 'index',
+    '/add', 'add',
 )
 
 app = web.application(urls, globals())
 
+db = web.database(dbn='sqlite', db='todo.db')
+
 class index:
     def GET(self):
-        name = 'Bob'
-        return render.index(name)
+        todos = db.select('todo')
+        return render.index(todos)
+
+class add:
+    def POST(self):
+        i = web.input()
+        n = db.insert('todo', title=i.title)
+        raise web.seeother('/')
 
 if __name__ == "__main__": app.run()
