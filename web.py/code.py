db9bf487989c7fbfe3cdd1dd1614be52be6c4910
Jianhong Liu <liujianhong1986@gmail.com>
2012-06-13 23:20:47 +0800

    simple-todo sqlite

--- web.py/code.py
+++ web.py/code.py
@@ -5,11 +5,20 @@ render = web.template.render('templates/')
 urls = (
     '/', 'index',
     '/add', 'add',
+    '/input', 'input',
+    '/.*', 'example',
 )
 
 app = web.application(urls, globals())
 
 db = web.database(dbn='sqlite', db='todo.db')
+
+class hello:
+    def GET(self):
+        return 'Hello, world!'
+class redirect:
+    def GET(self, path):
+        web.seeother('/' + path)
 
 class index:
     def GET(self):
@@ -22,4 +31,14 @@ class add:
         n = db.insert('todo', title=i.title)
         raise web.seeother('/')
 
+class example:
+    def GET(self):
+        referer = web.ctx.env.get('HTTP_REFERER', 'http://google.com')
+        print referer
+        raise web.seeother(referer)
+
+class input:
+    def GET(self):
+        user_data = web.input(id=[])
+        return "<h1>" +",".join(user_data.id) + "</h1>"
 if __name__ == "__main__": app.run()
